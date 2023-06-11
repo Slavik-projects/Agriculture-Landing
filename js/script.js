f6b295cf0cb98690b6b4dabc92483fde155c1ef5
Slavik-projects <vsfedotovua@gmail.com>
2023-06-11 21:11:25 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const nameVic = document.querySelector(".footer__victor");
+const file = document.querySelector(".file");
+nameVic.addEventListener("click", function showHideSubscribe(){
+      file.classList.toggle('active');
+})
